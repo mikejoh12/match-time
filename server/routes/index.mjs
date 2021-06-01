76097031cdddc9ed678e9f5d3ad8b286fee8ccff
Michael Johansson <mikejoh12@gmail.com>
2021-06-01 12:54:59 -0500

    add basic Swagger UI docs at /api-docs

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -2,5 +2,5 @@ import Router from 'express-promise-router'
 
 export const router = Router()
 
-router.get('/bookings', (req, res) =>
+router.get('/bookings/1', (req, res) =>
     res.send('Here are the bookings: booking1, booking2..'))
