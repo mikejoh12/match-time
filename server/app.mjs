1603772b3889adecd20e86a659615d315bf8bef3
Ed <76992905+edphan@users.noreply.github.com>
2021-06-01 12:16:16 -0700

    re-organize the initial client files

--- server/app.mjs
+++ server/app.mjs
@@ -1,24 +1,24 @@
-import express from 'express'
-import { router } from './routes/index.mjs'
-import cors from 'cors'
+import express from 'express';
+import { router } from './routes/index.mjs';
+import cors from 'cors';
 
-import YAML from 'yamljs'
-import swaggerUI from 'swagger-ui-express'
-const swaggerDocument = YAML.load('./openapi.yaml')
+import YAML from 'yamljs';
+import swaggerUI from 'swagger-ui-express';
+const swaggerDocument = YAML.load('./openapi.yaml');
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
 // Todo: secure cors settings
-app.use(cors())
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Calendar-booking API is running.')
-  })
+	res.send('Calendar-booking API is running.');
+});
 
-  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
-app.use('/api', router)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/api', router);
 
 app.listen(port, () => {
-console.log(`Calendar-booking API listening at http://localhost:${port}`)
-})
+	console.log(`Calendar-booking API listening at http://localhost:${port}`);
+});
