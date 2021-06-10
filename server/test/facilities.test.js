0057b9b83b4cdf9810a9af217a7c7d1950464828
Michael Johansson <mikejoh12@gmail.com>
2021-06-09 21:09:47 -0500

    facilities get and post tests

--- server/test/facilities.test.js
+++ server/test/facilities.test.js
@@ -2,8 +2,9 @@ const request = require('supertest')
 const app = require('../app')
 const { pool } = require('../config/config.js')
 
-describe('/api/facilities/{id}', () => {
-  before(async() => {
+describe('/api/facilities', () => {
+  
+  beforeEach(async() => {
     await pool.query(
       `CREATE TABLE "facilities" (
       "id" SERIAL PRIMARY KEY,
@@ -15,27 +16,62 @@ describe('/api/facilities/{id}', () => {
     )
   })
 
-  after(async () => {
+  afterEach(async () => {
     await pool.query(`DROP TABLE facilities`)
   })
+  
+  describe('GET /api/facilities', () => {
 
-  it('should respond with JSON and a 200 status code for a valid facility id', done => {
-    request(app)
-      .get('/api/facilities/1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
+    it('should respond with JSON and a 200 status code', done => {
+      request(app)
+        .get('/api/facilities/1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    })
   })
 
-  it('should respond with a 422 status code for an invalid facility id', done => {
-    request(app)
-      .get('/api/facilities/999')
-      .expect(422, done);
+  describe('GET /api/facilities/{id}', () => {
+
+    it('should respond with JSON and a 200 status code for a valid facility id', done => {
+      request(app)
+        .get('/api/facilities/1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    })
+
+    it('should respond with a 422 status code for an invalid facility id', done => {
+      request(app)
+        .get('/api/facilities/999')
+        .expect(422, done);
+    })
+
+    it('should respond with a 422 status code for a non-integer id', done => {
+      request(app)
+        .get('/api/facilities/test')
+        .expect(422, done);
+    })
   })
 
-  it('should respond with a 422 status code for a non-integer id', done => {
-    request(app)
-      .get('/api/facilities/test')
-      .expect(422, done);
+  describe('POST /api/facilities', () => {
+
+    it('should respond with a 201 status code when creating new facility', done => {
+      request(app)
+        .post('/api/facilities')
+        .send({name: 'Test Club', description: 'A great place'})
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201, done);
+    })
+
+    it('should respond with a 422 status code for missing facility info in request body', done => {
+      request(app)
+        .post('/api/facilities')
+        .send({})
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(422, done);
+    })
   })
 })
