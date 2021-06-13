d17ef9f107d52cec33cc3e86e517d62364ae0f42
Michael Johansson <mikejoh12@gmail.com>
2021-06-13 07:19:18 -0500

    get resources test and doc

---
+++ server/test/resources.test.js
@@ -0,0 +1,56 @@
+const request = require('supertest')
+const app = require('../app')
+const { pool } = require('../config/config.js')
+
+describe('/api/resources', () => {
+
+    beforeEach(async() => {
+        await pool.query(
+            `CREATE TABLE "facilities" (
+            "id" SERIAL PRIMARY KEY,
+            "name" varchar(100) NOT NULL,
+            "description" varchar(1000))`
+            )
+        await pool.query(
+            `INSERT INTO facilities (name, description) VALUES ('Smash Tennis Club', 'A private tennis club with well maintened indoor courts, hard courts, clay courts, and padel courts.')`
+            )
+        await pool.query(
+            `CREATE TABLE "resources" (
+                "id" SERIAL PRIMARY KEY,
+                "facilities_id" int,
+                "name" varchar(100),
+                "description" varchar(100)
+            );`
+        )
+        await pool.query(
+        `INSERT INTO resources (facilities_id, name, description) VALUES (1, 'Court 1', 'Indoor hard-court')`)
+    })
+
+    afterEach(async () => {
+        await pool.query(`DROP TABLE resources`)
+        await pool.query(`DROP TABLE facilities`)
+    })
+
+  describe('GET /api/resources/by_facility/{id}', () => {
+
+    it('should respond with JSON and a 200 status code for a valid facility id', done => {
+      request(app)
+        .get('/api/resources/by_facility/1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    })
+
+    it('should respond with a 422 status code for an invalid facility id', done => {
+      request(app)
+        .get('/api/resources/by_facility/999')
+        .expect(422, done);
+    })
+
+    it('should respond with a 422 status code for a non-integer id', done => {
+      request(app)
+        .get('/api/resources/by_facility/test')
+        .expect(422, done);
+    })
+  })
+})
