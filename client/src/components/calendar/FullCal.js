73034d5dbf132f832cfe6d4319f115a23deef2c2
Michael Johansson <mikejoh12@gmail.com>
2021-06-14 21:25:56 -0500

    connect events to calendars

--- client/src/components/calendar/FullCal.js
+++ client/src/components/calendar/FullCal.js
@@ -1,26 +1,45 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from "react-redux"
+import { fetchBookings, selectBookings } from "../../features/bookings/bookingsSlice"
+import { selectFacility } from "../../features/facilities/facilitiesSlice"
+import { fetchResources, selectResources } from '../../features/resources/resourcesSlice'
+
 import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid'
 
-export const FullCal = ({resources, bookings}) => {
-  
-    const resourceMap = resources.map(resource => ({
-        resourceId: resource.id,
-        resourceTitle: resource.name
-    }))
+export const FullCal = () => {
+    const dispatch = useDispatch()
+    const facility = useSelector(selectFacility)
+    const resources = useSelector(selectResources)
+    const bookings = useSelector(selectBookings)
 
-    const calendars = resourceMap.map(resource => 
+    useEffect(() => {
+      dispatch(fetchBookings(facility.id))
+      dispatch(fetchResources(facility.id))
+    }, [facility, dispatch])
+
+    const calendars = resources.map(resource => 
       <FullCalendar
-        key={resource.resourceId}
-        plugins={[dayGridPlugin, timeGridPlugin ]}
+        key={resource.id}
+        plugins={[ timeGridPlugin ]}
         headerToolbar={{
           left: '',
           center: 'title',
           right: ''
         }}
         initialView="timeGridDay"
+        slotMinTime="06:00:00"
+        slotMaxTime="22:00:00"
         height="auto"
-        initialDate="2011-10-05" />
+        initialDate="2011-10-05"
+        events={bookings[resource.id].map(booking => ({
+          id: booking.bookings_id,
+          title: `Booked by user: ${booking.organizer_id}`,
+          start: new Date(booking.start_time),
+          end: new Date(booking.end_time)
+        })
+
+        )} />
         )
 
     return (
--- client/src/components/dashboard/UserDashboard.js
+++ client/src/components/dashboard/UserDashboard.js
@@ -1,24 +1,9 @@
-import { useEffect } from 'react'
-import { useSelector, useDispatch } from "react-redux"
-import { fetchBookings, selectBookings } from "../../features/bookings/bookingsSlice"
-import { selectFacility } from "../../features/facilities/facilitiesSlice"
-import { fetchResources, selectResources } from '../../features/resources/resourcesSlice'
 import { FullCal } from '../calendar/FullCal'
 
 export const UserDashboard = () => {
 
-    const dispatch = useDispatch()
-    const facility = useSelector(selectFacility)
-    const bookings = useSelector(selectBookings)
-    const resources = useSelector(selectResources)
-
-    useEffect(() => {
-        dispatch(fetchBookings(facility.id))
-        dispatch(fetchResources(facility.id))
-    }, [facility, dispatch])
-
     return (
-            <FullCal resources={resources} bookings={bookings}/>
+            <FullCal />
       )
   }
   
