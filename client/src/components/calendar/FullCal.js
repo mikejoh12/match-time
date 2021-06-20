d7fc8ddf5f438fbd45c8b9d3280cf0d91b073273
Michael Johansson <mikejoh12@gmail.com>
2021-06-19 18:56:55 -0700

    access Full Cal API via useRef and functional comp

--- client/src/components/calendar/FullCal.js
+++ client/src/components/calendar/FullCal.js
@@ -1,12 +1,17 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from "react-redux"
 import { fetchBookings, selectBookings, selectFetchBookingsStatus, selectCalViewDate, calViewDateUpdated } from "../../features/bookings/bookingsSlice"
 import { selectFacility } from "../../features/facilities/facilitiesSlice"
 import { fetchResources, selectResources } from '../../features/resources/resourcesSlice'
-
+import 'date-fns';
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import timeGridPlugin from '@fullcalendar/timegrid'
-import { CalDatePicker } from './CalDatePicker'
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers'
 
 export const FullCal = () => {
     const dispatch = useDispatch()
@@ -16,6 +21,18 @@ export const FullCal = () => {
     const fetchBookingsStatus = useSelector(selectFetchBookingsStatus)
     const calViewDate = useSelector(selectCalViewDate)
 
+    // Create a React ref to be able to access Full Calendar API to set dates from external code
+    const calendarsRefs = useRef([])
+
+    const handleDateChange = (date) => {
+      dispatch(calViewDateUpdated(date.toISOString()))
+      console.log(`Selected date: ${date.toISOString()}`)
+      for (const cal of calendarsRefs.current) {
+        let calendarApi = cal.getApi()
+        calendarApi.gotoDate(date)
+      }
+    }
+
     useEffect(() => {
       dispatch(fetchBookings(facility.id))
       dispatch(fetchResources(facility.id))
@@ -23,6 +40,7 @@ export const FullCal = () => {
 
     const calendars = resources.map(resource => 
       <FullCalendar
+        ref={el => (calendarsRefs.current.push(el))}
         key={resource.id}
         plugins={[ timeGridPlugin ]}
         headerToolbar={{
@@ -47,7 +65,25 @@ export const FullCal = () => {
  
     return (
       <div style={{width: "300px", margin: "auto"}}>
-        <CalDatePicker />
+        
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Grid container justify="space-around">
+          <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="MM/dd/yyyy"
+            margin="normal"
+            id="date-picker-inline"
+            label="Select date:"
+            value={new Date(useSelector(selectCalViewDate))}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          </Grid>
+        </MuiPickersUtilsProvider>
+
         {fetchBookingsStatus === 'succeeded' ?
           calendars
           :
