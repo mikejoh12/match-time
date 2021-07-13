cdb851653006e9571b7871d001f8b85c047f7749
Michael Johansson <mikejoh12@gmail.com>
2021-07-13 12:45:42 -0500

    refactor resources get to RTK query

--- client/src/components/dashboard/ManagerFacilityEdit.js
+++ client/src/components/dashboard/ManagerFacilityEdit.js
@@ -6,45 +6,30 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
-import { selectResources, fetchResources, deleteResource } from '../../features/resources/resourcesSlice'
-import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
+import { deleteResource } from '../../features/resources/resourcesSlice'
+import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { AddResourceDialog } from './AddResourceDialog';
 import { DeleteFacilityDialog } from './DeleteFacilityDialog';
 import { useGetFacilityByIdQuery } from '../../services/api'
+import { useGetResourcesByFacilityIdQuery } from '../../services/api'
+
 
 export const ManagerFacilityEdit = () => {
-    const resources = useSelector(selectResources)
     const dispatch = useDispatch()
     const { id } = useParams()
-    const { data, isError, isLoading } = useGetFacilityByIdQuery(id)
-    const facility = data
-
-    useEffect(() => {
-        dispatch(fetchResources(id))
-        //dispatch(fetchFacility(id))
-      }, [id, dispatch])
+    const { data: facilityData, isError: facilityIsError, isLoading: facilityIsLoading } = useGetFacilityByIdQuery(id)
+    const { data: resourcesData, isError: resourcesIsError, isLoading: resourcesIsLoading } = useGetResourcesByFacilityIdQuery(id)
 
     const handleDeleteClick = event => dispatch(deleteResource(event.currentTarget.value))
 
-    const facilityResources = resources.map(resource => 
-        <ListItem key={resource.id} divider>
-            <ListItemText primary={`Name: ${resource.name} - Description: ${resource.description}`} />
-            <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label="delete" value={resource.id} onClick={handleDeleteClick}>
-                    <DeleteIcon />
-                </IconButton>
-            </ListItemSecondaryAction>
-        </ListItem>)
-
     return (
         <div>
-            {isError ? (
+            {facilityIsError || resourcesIsError? (
             <>Oh no, there was an error</>
-            ) : isLoading ? (
+            ) : facilityIsLoading || resourcesIsLoading? (
             <>Loading...</>
-            ) : data ? (
+            ) : facilityData && resourcesData? (
                 <Grid   container
                 spacing={2}
                 direction="column"
@@ -57,17 +42,25 @@ export const ManagerFacilityEdit = () => {
                     </Grid>
                     <Grid item>
                         <Typography variant="h6" >
-                            Name: {facility.name}
+                            Name: {facilityData.name}
                         </Typography>              
                     </Grid>
                     <Grid item>
                         <Typography variant="h6" >
-                            Description: {facility.description}
+                            Description: {facilityData.description}
                         </Typography>              
                     </Grid>
                     <Grid item>
                             <List component="nav" aria-label="user bookings">
-                                {facilityResources}
+                                {resourcesData.map(resource => 
+                                    <ListItem key={resource.id} divider>
+                                        <ListItemText primary={`Name: ${resource.name} - Description: ${resource.description}`} />
+                                        <ListItemSecondaryAction>
+                                            <IconButton edge="end" aria-label="delete" value={resource.id} onClick={handleDeleteClick}>
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </ListItemSecondaryAction>
+                                    </ListItem>)}
                             </List>
                     </Grid>
                     <Grid item>
