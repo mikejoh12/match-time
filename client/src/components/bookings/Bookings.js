da164b429ce33ed4520e9adcabf70c287496f258
Michael Johansson <mikejoh12@gmail.com>
2021-08-22 13:44:07 -0500

    remove unused imports

--- client/src/components/bookings/Bookings.js
+++ client/src/components/bookings/Bookings.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux'
 import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import DeleteIcon from '@material-ui/icons/Delete';
-import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from "@material-ui/core/Grid";
